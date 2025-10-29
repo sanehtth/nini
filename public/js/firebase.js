0f9e43dd9de18715f0bc018930cf35b70ddf9a62
sanehtth <sane.htth@gmail.com>
2025-10-29 10:02:39 +0700

    Update firebase.js

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -1,21 +1,19 @@
-﻿// js/firebase.js
-const firebaseConfig = {
-  // DÁN CONFIG CỦA BẠN TỪ FIREBASE CONSOLE
-   apiKey: "AIzaSyDm8achECq0QXN34dp3cLflKzS8Ge78R5E",
+// window.App namespace để tránh trùng
+window.App = window.App || {};
+
+(() => {
+  const firebaseConfig = {
+    apiKey: "AIzaSyDm8achECq0QXN34dp3cLflKzS8Ge78R5E",
   authDomain: "nini-funny.firebaseapp.com",
   databaseURL: "https://nini-funny-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "nini-funny",
   storageBucket: "nini-funny.firebasestorage.app",
   messagingSenderId: "158986131827",
   appId: "1:158986131827:web:92e9a212747582d83d4d06"
-};
+  };
 
-// Khởi tạo Firebase
-firebase.initializeApp(firebaseConfig);
-const auth = firebase.auth();
-const db = firebase.database();
+  firebase.initializeApp(firebaseConfig);
 
-// Xuất biến để main.js dùng
-window.firebaseAuth = auth;
-
-window.firebaseDB = db;
+  window.App.auth = firebase.auth();
+  window.App.db   = firebase.database();
+})();
