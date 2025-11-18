79b3460996ace045b4b0fe4b627bfced3083ca84
sanehtth <sane.htth@gmail.com>
2025-11-19 04:04:46 +0700

    Add files via upload

---
+++ public/js/xpDebug.js
@@ -0,0 +1,13 @@
+﻿// xpDebug – log lịch sử XP/Coin
+window.XP_DEBUG = window.XP_DEBUG || [];
+
+window.logXPChange = function (source, xp, coin) {
+  const entry = {
+    source,          // tên file + vị trí
+    xp,
+    coin,
+    time: new Date().toISOString()
+  };
+  window.XP_DEBUG.push(entry);
+  console.log("[XP-DEBUG]", entry);
+};
